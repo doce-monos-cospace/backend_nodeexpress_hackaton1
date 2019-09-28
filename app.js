7ec7c5b83f5a519a0721f37f5e0aa6cc2782efa7
raskardev <alexcdev95@gmail.com>
2019-09-28 09:27:11 +0200

    feat(): add mongo connection and initial dummy domain

--- app.js
+++ app.js
@@ -11,5 +11,6 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+require('./db');
 
 app.listen(3000, () => console.log('Servidor iniciado en el puerto 3000'));
--- db/index.js
+++ db/index.js
@@ -1,3 +1,19 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = require('bluebird');
+const start = mongoose.connect(
+  'mongodb+srv://docemonos:pass1234@stridermongo-4ef3j.mongodb.net/test',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.error(err.message);
+      throw err;
+    }
+
+    console.log('MongoDB conectado');
+  },
+);
+
+// Aquí se exportan los modelos
+module.exports = {
+  start,
+};
