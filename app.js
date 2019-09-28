eefc03629b1b0588fc724d032f68026cd796d53a
acopero <acopero@soamee.com>
2019-09-28 09:11:12 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.listen(3000, () => console.log('Servidor iniciado en el puerto 3000'));
